fb01e4908a607f99054d8d669f97625797bb984c
msingh189 <singhrajawatm@gmail.com>
2018-12-27 18:00:19 +0530

    There was problem in previous way, so I started again freshly.

---
+++ app/src/main/java/com/example/msingh189/newsroom/ClubsFragment.java
@@ -0,0 +1,17 @@
+package com.example.msingh189.newsroom;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ClubsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_clubs, container, false);
+    }
+}
